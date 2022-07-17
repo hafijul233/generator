190cd66bf8d943fa57b3afb86a43c6069d05ce63
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-07-18 01:42:08 +0600

    table ui working

--- config/generator.php
+++ config/generator.php
@@ -24,4 +24,50 @@ return [
 
         'default' => 'web'
     ],
+
+    /**
+     *
+     */
+    'model_options' => [
+        'soft-delete' => 'Soft Delete',
+        'audit' => 'Change Audit',
+        'migration' => 'Create Table Migration',
+        'factory' => 'Create Faker Factory Class'
+    ],
+
+    /**
+     *
+     */
+    'model_defaults' => [
+        'soft-delete',
+        'audit',
+        'migration',
+    ],
+
+    /**
+     *
+     */
+    'field_types' => [
+        'Text Fields' => [
+            'text' => 'Text',
+            'email' => 'Email',
+            'password' => 'Password',
+        ],
+        'Number Fields' => [
+            'integer' => 'Integer',
+            'float' => 'float',
+            'money' => 'Money',
+        ],
+        'Date Time Fields' => [
+            'date' => 'Only Date',
+            'time' => 'Only Time',
+            'datetime' => 'Date Time',
+        ],
+        'File Fields' => [
+            'file' => 'Default file uploader',
+            'image' => 'Image uploader with preview',
+        ],
+        'Relation Fields' => [
+        ],
+    ]
 ];
