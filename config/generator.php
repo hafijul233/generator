762a1e6c338060d11117d181852e46e14f6315cd
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-07-18 10:13:37 +0600

    new field added

--- config/generator.php
+++ config/generator.php
@@ -52,16 +52,33 @@ return [
             'text' => 'Text',
             'email' => 'Email',
             'password' => 'Password',
+            'url' => 'Website URL',
+
+            'textarea' => 'Large Text (Textarea)'
+        ],
+        'Checkbox/Radio Fields' => [
+            'checkbox' => 'Checkbox',
+            'radio' => 'Radio',
+            'select' => 'Select',
+            'multi-select' => 'Multiple Select',
+
         ],
         'Number Fields' => [
             'integer' => 'Integer',
             'float' => 'float',
             'money' => 'Money',
+            'mobile' => 'Mobile',
         ],
         'Date Time Fields' => [
             'date' => 'Only Date',
             'time' => 'Only Time',
             'datetime' => 'Date Time',
+        ],
+        'Slider Dropdown Fields' => [
+            'range' => 'Range Slider',
+            'range-dropdown' => 'Select Range Dropdown',
+            'password' => 'Password',
+            'textarea' => 'Large Text (Textarea)'
         ],
         'File Fields' => [
             'file' => 'Default file uploader',
